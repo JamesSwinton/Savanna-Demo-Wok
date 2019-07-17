ceb19a881b9be9987465cbb604e6eb1c637b0281
JS6845 <JS6845@zebra.com>
2019-07-17 13:49:33 +0100

    Retrofit singleton

---
+++ app/src/main/java/com/zebra/jamesswinton/savannademowok/network/RetrofitInstance.java
@@ -0,0 +1,22 @@
+package com.zebra.jamesswinton.savannademowok.network;
+
+import com.google.gson.GsonBuilder;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+  private static Retrofit retrofitInstance = null;
+  private static final String BASE_URL = "https://api.zebra.com/v2/tools/";
+
+  public static Retrofit getInstance() {
+    if (retrofitInstance == null) {
+      retrofitInstance = new Retrofit.Builder()
+          .baseUrl(BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
+          .build();
+    } return retrofitInstance;
+  }
+
+  private RetrofitInstance() { }
+}
