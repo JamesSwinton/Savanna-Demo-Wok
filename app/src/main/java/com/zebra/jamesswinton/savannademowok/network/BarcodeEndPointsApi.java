de77c7a3dbef310b9ce0173cb4195a0af2c414ff
JS6845 <JS6845@zebra.com>
2019-07-17 13:49:25 +0100

    Barcode Intelligence API interfaces

---
+++ app/src/main/java/com/zebra/jamesswinton/savannademowok/network/BarcodeEndPointsApi.java
@@ -0,0 +1,31 @@
+package com.zebra.jamesswinton.savannademowok.network;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+public interface BarcodeEndPointsApi {
+
+  // Create Barcode
+  @GET("barcode/generate/")
+  Call<ResponseBody> createBarcode(
+      @Header("apikey") String apiKey,
+      @Query("symbology") String symbology,
+      @Query("text") String text,
+      @Query("scaleX") Integer scaleX,
+      @Query("scaleY") Integer scaleY,
+      @Query("scaleX") Integer scale,
+      @Query("rotate") String rotate,
+      @Query("includeText") String includeText
+  );
+
+  // Barcode Lookup
+//  @GET("barcode/lookup/")
+//  Call<UpcProduct> findBarcode(
+//      @Header("apikey") String apiKey,
+//      @Query("upc") String barcode
+//  );
+
+}
