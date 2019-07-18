ba6d5035231798327575fdae247d885734376ec6
JS6845 <JS6845@zebra.com>
2019-07-18 10:01:56 +0100

    Utility Class for checking & requesting permissions

---
+++ app/src/main/java/com/zebra/jamesswinton/savannademowok/PermissionsHelper.java
@@ -0,0 +1,18 @@
+package com.zebra.jamesswinton.savannademowok;
+
+import android.app.Activity;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class PermissionsHelper {
+
+  public static boolean hasPermission(Activity context, String[] permissions, String rational) {
+    if (EasyPermissions.hasPermissions(context, permissions)) {
+      return true;
+    } else {
+      // Do not have permissions, request them now
+      EasyPermissions.requestPermissions(context, rational, 0, permissions);
+      return false;
+    }
+  }
+
+}
