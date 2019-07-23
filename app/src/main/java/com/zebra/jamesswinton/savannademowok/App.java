4d6aba76178ff993c725c2983d88512c7a6d35fd
JS6845 <JS6845@zebra.com>
2019-07-23 16:30:38 +0100

    Added preference manager & wrapper method to Application class

--- app/src/main/java/com/zebra/jamesswinton/savannademowok/App.java
+++ app/src/main/java/com/zebra/jamesswinton/savannademowok/App.java
@@ -1,6 +1,8 @@
 package com.zebra.jamesswinton.savannademowok;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
 import com.zebra.jamesswinton.savannademowok.scanning.DataWedgeUtilities;
 
 public class App extends Application {
@@ -9,13 +11,13 @@ public class App extends Application {
   private static final String TAG = "ApplicationClass";
 
   // Constants
+  private static final String API_KEY_PREF = "api_key";
 
 
   // Static Variables
 
-
   // Variables
-
+  private static SharedPreferences mSharedPreferences;
 
   @Override
   public void onCreate() {
@@ -23,6 +25,9 @@ public class App extends Application {
 
     // Init DataWedge
     initDataWedge();
+
+    // Init Preferences
+    initPreferences();
   }
 
   private void initDataWedge() {
@@ -30,5 +35,20 @@ public class App extends Application {
     DataWedgeUtilities.CreateProfile(getApplicationContext());
     // Set Profile Configuration
     DataWedgeUtilities.SetProfileConfig(this);
+  }
+
+  /**
+   * Preference Helper Methods
+   */
+
+  private void initPreferences() {
+    // Init Shared Prefs
+    if (mSharedPreferences == null) {
+      mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+    }
+  }
+
+  public static String getApiKey() {
+    return mSharedPreferences.getString(API_KEY_PREF, "");
   }
 }
