5831e13ced980e37e2ef78c2e1620406d3b119c7
JS6845 <JS6845@zebra.com>
2019-09-03 12:45:11 +0100

    Removed unused imports

--- app/src/main/java/com/zebra/jamesswinton/savannademowok/apis/barcodeintelligence/UpcProductImagesAdapter.java
+++ app/src/main/java/com/zebra/jamesswinton/savannademowok/apis/barcodeintelligence/UpcProductImagesAdapter.java
@@ -1,12 +1,10 @@
-package com.zebra.jamesswinton.savannademowok.barcodeintelligence;
+package com.zebra.jamesswinton.savannademowok.apis.barcodeintelligence;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
