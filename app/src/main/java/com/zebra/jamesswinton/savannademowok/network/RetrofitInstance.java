78db01db238c7c9e5dca93d920b68121e3328e13
JS6845 <JS6845@zebra.com>
2019-09-03 12:39:07 +0100

    Added Sandbox instance to Singleton to support multiple endpoints

--- app/src/main/java/com/zebra/jamesswinton/savannademowok/network/RetrofitInstance.java
+++ app/src/main/java/com/zebra/jamesswinton/savannademowok/network/RetrofitInstance.java
@@ -7,7 +7,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
   private static Retrofit retrofitInstance = null;
-  private static final String BASE_URL = "https://api.zebra.com/v2/tools/";
+  private static Retrofit retrofitInstanceSandbox = null;
+  private static final String BASE_URL = "https://api.zebra.com/v2/";
+  private static final String BASE_URL_SANDBOX = "https://sandbox-api.zebra.com/v2/";
 
   public static Retrofit getInstance() {
     if (retrofitInstance == null) {
@@ -16,6 +18,15 @@ public class RetrofitInstance {
           .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
           .build();
     } return retrofitInstance;
+  }
+
+  public static Retrofit getInstanceSandbox() {
+    if (retrofitInstanceSandbox == null) {
+      retrofitInstanceSandbox = new Retrofit.Builder()
+          .baseUrl(BASE_URL_SANDBOX)
+          .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
+          .build();
+    } return retrofitInstanceSandbox;
   }
 
   private RetrofitInstance() { }
